2a1ff470af49167480a8e9b50d2bc2107c47c766
Denis Slapelis <deniswesleyss@hotmail.com>
2020-04-24 22:03:19 -0300

    Updates

---
+++ precache-manifest.eddab5db06fb5ec9fbb3e5b310329326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54447aba864f392505968844fab6ffe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dd625c7f7ab9aeb71ec5",
+    "url": "/monsters-rolodex/static/css/main.fa1ce2af.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd625c7f7ab9aeb71ec5",
+    "url": "/monsters-rolodex/static/js/main.fcb09ff9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
